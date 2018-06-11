ef48f94791789debdae00a2d231ac24405bf0595
student <student@IBLAB732P-13.ib.polsl.pl>
2018-06-11 14:37:15 +0200

    quicksort

--- src/sort/methods/Quicksort.java
+++ src/sort/methods/Quicksort.java
@@ -11,4 +11,32 @@ package sort.methods;
  */
 public class Quicksort {
     /* http://www.algorytm.org/algorytmy-sortowania/sortowanie-szybkie-quicksort/quick-j.html */
+    private static void quicksort(double tablica[], int x, int y) {
+ 
+    int i,j;
+    double v,temp;
+    
+    i=x;
+    j=y;
+    v=tablica[(x+y) / 2];
+    do {
+    while (tablica[i]<v)
+    i++;
+    while (v<tablica[j])
+    j--;
+    if (i<=j) {
+    temp=tablica[i];
+    tablica[i]=tablica[j];
+    tablica[j]=temp;
+    i++;
+    j--;
+    }
+}   
+while (i<=j);
+    
+if (x<j)
+    quicksort(tablica,x,j);
+if (i<y)
+    quicksort(tablica,i,y);
+}
 }
