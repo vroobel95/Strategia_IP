49ccfb88e1cd4eb6dea52484a955e63b5fdfa8c0
student <student@IBLAB732P-13.ib.polsl.pl>
2018-06-11 14:34:11 +0200

    bubblesort

--- src/sort/methods/Bubblesort.java
+++ src/sort/methods/Bubblesort.java
@@ -11,4 +11,24 @@ package sort.methods;
  */
 public class Bubblesort {
     /* http://www.algorytm.org/algorytmy-sortowania/sortowanie-babelkowe-bubblesort.html */
+    private static void b_sort(double tab[]){
+        double temp;
+        int zmiana = 1;
+        while(zmiana > 0){
+        zmiana = 0;
+        
+        for(int i=0; i<tab.length-1; i++){
+            if(tab[i]>tab[i+1]){
+            temp = tab[i+1];
+            tab[i+1] = tab[i];
+            tab[i] = temp;
+            zmiana++;
+            }
+        }
+    }
+        
+        for(int i=0; i<tab.length; i++){
+        System.out.print(tab[i]+" ");
+        }
+    }
 }
